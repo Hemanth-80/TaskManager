7f74947136849d1313ec8f7f92525e12f09ba710
Hemanth-80 <banothhemanth80@gmail.com>
2026-05-07 17:53:24 +0530

    Add files via upload

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+import ManagerDashboard from "./ManagerDashboard";
+import TeamMemberDashboard from "./TeamMemberDashboard";
+
+export default function Dashboard() {
+  const role = sessionStorage.getItem("role");
+
+  
+if (role === null) {
+  return null;
+}
+
+if (!role) {
+  return <Navigate to="/login" />;
+}
+
+  return role === "manager"
+    ? <ManagerDashboard />
+    : <TeamMemberDashboard />;
+}
